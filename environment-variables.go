54548b26114fd545cb393eba19e4d1014679df17
Glenn Gillen <me@glenngillen.com>
2014-03-27 11:50:03 -0700

    Turn env vars into JSON object.

--- environment-variables.go
+++ environment-variables.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
+	envMap := make(map[string]string)
 	for _, e := range os.Environ() {
 		parts := strings.SplitN(e, "=", 2)
 		key := parts[0]
 		val := parts[1]
+		envMap[key] = val
 		fmt.Printf("'%s' has a value of '%s'\n", key, val)
 	}
+
+	envJson, err := json.Marshal(envMap)
+	if err != nil { panic(err) }
+	fmt.Println(string(envJson))
 }

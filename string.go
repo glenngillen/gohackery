7e24709c46a13787d128e34f8c79813421bf0632
Glenn Gillen <me@glenngillen.com>
2014-03-27 10:35:37 -0700

    Build up a string from a slice.

--- string.go
+++ string.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"strings"
+)
+
 func main() {
 	s := "This"
 	s += " is"
 	s += " a"
 	s += " sentence."
 	println(s)
+
+	sSlice := make([]string, 0)
+	sSlice = append(sSlice, "And")
+	sSlice = append(sSlice, "a")
+	sSlice = append(sSlice, "second")
+	sSlice = append(sSlice, "made")
+	sSlice = append(sSlice, "of")
+	sSlice = append(sSlice, "slices.")
+
+	println(strings.Join(sSlice, " "))
 }

88e4d9a7e88808be9268a7ac530e4bccaad0c86f
Glenn Gillen <me@glenngillen.com>
2014-03-27 11:40:11 -0700

    Iterate of environment variables and output keys/vals.

--- environment-variables.go
+++ environment-variables.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, e := range os.Environ() {
-		println(e)
+		parts := strings.SplitN(e, "=", 2)
+		key := parts[0]
+		val := parts[1]
+		fmt.Printf("'%s' has a value of '%s'\n", key, val)
 	}
 }

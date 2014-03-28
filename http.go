c934b0bf8993dcf163519b9da6092d6eebf42250
Glenn Gillen <me@glenngillen.com>
2014-03-27 17:34:11 -0700

    Do a HTTP GET request.

---
+++ http.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://www.google.com/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}

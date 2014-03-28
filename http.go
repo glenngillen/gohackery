2415cbd316ba4595390f94ccc043aa902c76d77f
Glenn Gillen <me@glenngillen.com>
2014-03-27 17:44:34 -0700

    Do a HTTP POST.

--- http.go
+++ http.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -16,4 +17,15 @@ func main() {
 		panic(err)
 	}
 	println(string(body))
+
+	postResp, err := http.PostForm("http://requestb.in/s5rh8ds5",
+		url.Values{"name": {"Glenn Gillen"}, "age": {"32"}})
+	if err != nil {
+		panic(err)
+	}
+	postBody, err := ioutil.ReadAll(postResp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(postBody))
 }
